b5b6997fc2d3ed93edbea778b7e3fee30cfc9a05
Indexyz <jiduye@gmail.com>
2021-06-01 20:11:18 +0800

    feat: add package

--- flake.nix
+++ flake.nix
@@ -5,9 +5,16 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-    nixosModules.indexyz = { ... }: {
-      imports = [ ./modules/all-modules.nix ];
+  outputs = { self, nixpkgs }:
+    let packages = {
+      linuxPackages_latest_chromeos = nixpkgs.callPackage ./packages/chromeos-kernel.nix;
     };
-  };
+    in
+    {
+      legacyPackages."x86_64-linux" = packages;
+      overlay = final: prev: packages;
+      nixosModules.indexyz = { ... }: {
+        imports = [ ./modules/all-modules.nix ];
+      };
+    };
 }
