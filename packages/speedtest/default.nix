7c2388ae5fad89c5fcf1f791ac3da1298a7f59b3
Indexyz <jiduye@gmail.com>
2021-06-13 20:54:18 +0000

    feat(package/speedtest): init

---
+++ packages/speedtest/default.nix
@@ -0,0 +1,25 @@
+{ stdenv, fetchurl }:
+
+stdenv.mkDerivation rec {
+  pname = "speedtest-cli";
+  version = "1.0.0";
+
+  src = fetchurl {
+    url = "https://bintray.com/ookla/download/download_file?file_path=ookla-speedtest-${version}-x86_64-linux.tgz";
+    sha256 = "0s1icdqdkr1g4dsnsqvh1a4vp4b61vvrr7qx4gsf89s41n7h5qjz";
+    name = "${pname}-${version}.tar.gz";
+  };
+
+  setSourceRoot = "sourceRoot=`pwd`";
+
+  meta.description = "CLI for SpeedTest";
+
+  buildPhase = ''
+    tar zxvf $src
+  '';
+
+  installPhase = ''
+    mkdir -p "$out"/bin/
+    install -m 755 speedtest -t "$out"/bin/
+  '';
+}
