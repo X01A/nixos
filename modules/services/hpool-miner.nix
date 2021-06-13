45ecdc8f3259aad1439a5fc389b0773d8765c455
Indexyz <jiduye@gmail.com>
2021-06-13 12:39:30 +0000

    feat(module/hpool-miner): init

---
+++ modules/services/hpool-miner.nix
@@ -0,0 +1,73 @@
+{ pkgs, config, lib, ... }:
+
+with lib;
+let
+  cfg = config.indexyz.services.hpool-miner;
+  minerConfig = {
+    apiKey = cfg.token;
+    deviceId = "";
+    cachePath = "";
+    debug = "";
+    log = {
+      lv = "info";
+      path = "./log";
+      name = "miner.log";
+    };
+    scanPath = true;
+    scanMinute = 1;
+    token = "";
+    minerName = cfg.name;
+    path = cfg.path;
+    extraParams = { };
+    url = {
+      info = "";
+      submit = "";
+    };
+  };
+
+  configData = pkgs.writeText "config.yaml" (builtins.toJSON minerConfig);
+in
+{
+  options = {
+    indexyz.services.hpool-miner = {
+      enable = mkOption {
+        default = false;
+        type = types.bool;
+      };
+
+      path = mkOption {
+        type = with types; listOf str;
+      };
+
+      name = mkOption {
+        type = types.str;
+      };
+
+      token = mkOption {
+        type = types.str;
+      };
+
+      dataDir = mkOption {
+        default = "/var/lib/hpool-miner";
+        type = types.str;
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    systemd.services.hpool-miner = {
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+      serviceConfig = {
+        Restart = "always";
+        RestartSec = "10s";
+      };
+      script = ''
+        mkdir -p ${cfg.dataDir}
+        cd ${cfg.dataDir}
+        cp ${configData} config.yaml
+        ${pkgs.hpool-miner-chia}/bin/hpool-miner-chia
+      '';
+    };
+  };
+}
