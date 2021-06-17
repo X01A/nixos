ce35eb5819e6de0aaff8ddd382419ec0c8dd4246
Indexyz <jiduye@gmail.com>
2021-06-17 03:51:38 +0000

    fix(package/transmission-web): syntax error

--- packages/transmission-web-control/default.nix
+++ packages/transmission-web-control/default.nix
@@ -5,4 +5,4 @@ fetchFromGitHub {
   repo = "transmission-web-control";
   rev = "4e0c781669af2f27d959dce80cb4956303375568";
   sha256 = "0g7zscbs741lnfi0gx43354lfmis1s5nsxdh3ndisvdq0dl6lz24";
-};
+}
