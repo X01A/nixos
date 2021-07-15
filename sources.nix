84b2897928cc9d8b829733d66fb53caaf6662a0a
Indexyz <jiduye@gmail.com>
2021-07-15 17:47:53 +0000

    package: yacd: ∅ → v0.3.2

--- sources.nix
+++ sources.nix
@@ -25,4 +25,16 @@
       sha256 = "0vvjywsigh9j669zd2blsnhlz5kjv4j39g33hclj2a7i44dwg13h";
     };
   };
+  yacd = {
+    pname = "yacd";
+    version = "v0.3.2";
+    src = fetchgit {
+      url = "https://github.com/haishanh/yacd";
+      rev = "v0.3.2";
+      fetchSubmodules = true;
+      deepClone = false;
+      leaveDotGit = false;
+      sha256 = "1jamzci2d2q3cbanbbjgyv83kc3mgr3sm86wzgl2li9ig3b6cqkz";
+    };
+  };
 }
