d96ac757780b5d4c4555d71cfbe9b0a201c9a69e
Indexyz <jiduye@gmail.com>
2021-07-15 10:23:13 +0000

    fix: updateConfigScript use full path

--- modules/services/clash/utils.nix
+++ modules/services/clash/utils.nix
@@ -8,24 +8,28 @@ let
 in
 rec {
   inherit managePort manageAddr;
-  updateConfigScript = name: enableUpdate: ''
-    # Replace ports
-    ${pkgs.yq-go}/bin/yq eval "del(.socks-port)" -i ${name}.yaml
-    ${pkgs.yq-go}/bin/yq eval "del(.port)" -i ${name}.yaml
-    ${pkgs.yq-go}/bin/yq eval ".mixed-port = ${toString cfg.port}" -i ${name}.yaml
+  updateConfigScript = name: enableUpdate:
+    let
+      fullName = "${cfg.dataDir}/${name}.yaml";
+    in
+    ''
+      # Replace ports
+      ${pkgs.yq-go}/bin/yq eval "del(.socks-port)" -i ${fullName}
+      ${pkgs.yq-go}/bin/yq eval "del(.port)" -i ${fullName}
+      ${pkgs.yq-go}/bin/yq eval ".mixed-port = ${toString cfg.port}" -i ${fullName}
 
-    ${optionalString cfg.allowLan ''
-    ${pkgs.yq-go}/bin/yq eval ".allow-lan = true" -i ${name}.yaml
-    ''}
+      ${optionalString cfg.allowLan ''
+      ${pkgs.yq-go}/bin/yq eval ".allow-lan = true" -i ${fullName}
+      ''}
 
-    ${pkgs.yq-go}/bin/yq eval ".dns.default-nameserver[0] = \"${cfg.dns}\"" -i ${name}.yaml
+      ${pkgs.yq-go}/bin/yq eval ".dns.default-nameserver[0] = \"${cfg.dns}\"" -i ${fullName}
 
-    # Format document
-    ${pkgs.yq-go}/bin/yq eval ${name}.yaml -I 4 --prettyPrint -i
+      # Format document
+      ${pkgs.yq-go}/bin/yq eval ${fullName} -I 4 --prettyPrint -i
 
-    ${optionalString ((name == cfg.config) && enableUpdate) ''
-    ${pkgs.curl}/bin/curl ${manageAddr}/configs -X PUT \
-        --data '{"path": "${cfg.dataDir}/${name}.yaml"}' ${optionalString (cfg.secret != null) "-H 'Authorization: Bearer ${cfg.secret}'"}
-    ''}
-  '';
+      ${optionalString ((name == cfg.config) && enableUpdate) ''
+      ${pkgs.curl}/bin/curl ${manageAddr}/configs -X PUT \
+          --data '{"path": "${fullName}"}' ${optionalString (cfg.secret != null) "-H 'Authorization: Bearer ${cfg.secret}'"}
+      ''}
+    '';
 }
