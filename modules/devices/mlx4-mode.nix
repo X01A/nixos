a55b31bacaeff9ce4df63b7efa637830c10285f4
Indexyz <jiduye@gmail.com>
2021-07-19 18:01:09 +0000

    feat: add mlx4 mode select device

---
+++ modules/devices/mlx4-mode.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, lib, ... }:
+
+
+with lib;
+let
+  cfg = config.indexyz.devices.mlx4-mode;
+in
+{
+  options = {
+    indexyz.devices.mlx4-mode = {
+      enable = mkEnableOption "Enable mlx4 mode switch";
+
+      mode = mkOption {
+        type = types.enum [ "eth" "ib" "auto" ];
+        default = "eth";
+        description = "Network card mode";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    boot.initrd = {
+      availableKernelModules = [ "mlx4_core" "mlx4_en" ];
+      postDeviceCommands = ''
+        for pci_dev in /sys/bus/pci/devices/*/; do
+          if [ -e $pci_dev/mlx4_port1 ]; then
+            echo "${cfg.mode}" > $pci_dev/mlx4_port1;
+            if [ -e $pci_dev/mlx4_port2 ]; then
+              echo "${cfg.mode}" > $pci_dev/mlx4_port2
+            fi
+          fi
+        done
+      '';
+    };
+  };
+}
