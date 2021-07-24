6aceca8bc710c1c86b7093fbf872ab151f2fab4f
Indexyz <jiduye@gmail.com>
2021-07-24 04:50:08 +0000

    feat: update config

---
+++ modules/desktops/font.nix
@@ -0,0 +1,59 @@
+{ config, pkgs, lib, ... }:
+
+with lib;
+let
+  cfg = config.indexyz.desktops.font;
+in
+{
+  options = {
+    indexyz.desktops.font = {
+      enable = mkEnableOption "Enable desktop fonts";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    fonts = {
+      enableDefaultFonts = true;
+      enableGhostscriptFonts = true;
+
+      fonts = with pkgs; [
+        roboto
+        jetbrains-mono
+        noto-fonts
+        noto-fonts-cjk
+        noto-fonts-extra
+        noto-fonts-emoji
+        wqy_microhei
+        wqy_zenhei
+        cascadia-code
+        iosevka
+      ];
+
+      fontconfig = {
+        enable = true;
+        localConf = ''
+          <alias>
+            <family>sans-serif</family>
+            <prefer>
+              <family>Noto Sans Display</family>
+              <family>Noto Sans CJK SC</family>
+              <family>Noto Sans CJK HK</family>
+              <family>Noto Sans CJK JP</family>
+              <family>Noto Sans CJK KR</family>
+              <family>Noto Sans CJK TC</family>
+            </prefer>
+          </alias>
+
+          <alias>
+            <family>monospace</family>
+            <prefer>
+              <family>Noto Sans Mono CJK SC</family>
+              <family>Noto Sans Mono CJK TC</family>
+              <family>Noto Sans Mono CJK JP</family>
+            </prefer>
+          </alias>
+        '';
+      };
+    };
+  };
+}
