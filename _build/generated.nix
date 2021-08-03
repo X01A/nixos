28ce00851877bd5ab1a1767347ac6c9f4fcb743c
Indexyz <jiduye@gmail.com>
2021-08-03 13:13:56 +0000

    fix(pkgs/yacd): move to pre-built

--- _build/generated.nix
+++ _build/generated.nix
@@ -130,13 +130,9 @@
   yacd = {
     pname = "yacd";
     version = "v0.3.3";
-    src = fetchgit {
-      url = "https://github.com/haishanh/yacd";
-      rev = "v0.3.3";
-      fetchSubmodules = true;
-      deepClone = false;
-      leaveDotGit = false;
-      sha256 = "0b2rwnn8rffnhhdmhjmd8wanxkslivnpxxs9y4k7b7h1abmgc6j4";
+    src = fetchurl {
+      sha256 = "1f5qdj22wplj5xw2hp9r8acq740csv4n4viwb39cyk77baxq8bvl";
+      url = "https://github.com/haishanh/yacd/releases/download/v0.3.3/yacd.tar.xz";
     };
 
   };
--- packages/yacd/default.nix
+++ packages/yacd/default.nix
@@ -1,31 +1,10 @@
-{ stdenv, source, yarn, mkYarnModules, nodejs-12_x, esbuild }:
+{ source, stdenv }:
 
 stdenv.mkDerivation rec {
   inherit (source) pname version src;
 
-  nodeModules = mkYarnModules {
-    name = "${pname}-node-modules-${version}";
-    inherit pname version;
-    # it is vitally important the the package.json has name and version fields
-    packageJSON = "${src}/package.json";
-    yarnLock = "${src}/yarn.lock";
-  };
-
-  buildInputs = [ nodejs-12_x yarn ];
-
-  # Fix esbuild
-  # https://discourse.nixos.org/t/building-a-sveltekit-app-with-nix/13002
-  configurePhase = ''
-    cp -r  ${nodeModules}/node_modules .
-    chmod -R +w node_modules
-    ln -sf ${esbuild}/bin/esbuild node_modules/esbuild/bin/esbuild
-  '';
-
-  buildPhase = ''
-    yarn run --offline build
-  '';
-
   installPhase = ''
-    cp -r public $out
+    mkdir $out
+    tar -xf $src --strip-components=1 -C $out
   '';
 }
