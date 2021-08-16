a4aa3b02d5d109a5c932f7f36b9451f55075d745
Indexyz <jiduye@gmail.com>
2021-08-16 10:16:13 +0800

    feat(services): add influxdb services

---
+++ modules/services/influxdb.nix
@@ -0,0 +1,48 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+let
+  cfg = config.indexyz.services.influxdb;
+in
+{
+  options = {
+    indexyz.services.influxdb = {
+      enable = mkEnableOption "Enable influxdb version 2";
+
+      dataDir = mkOption {
+        default = "/var/lib/influxdb";
+        type = types.str;
+      };
+
+      bindAddress = mkOption {
+        type = types.str;
+        default = ":8086";
+        description = "Bind address for the REST HTTP API";
+      };
+
+      extraArgs = mkOption {
+        type = types.str;
+        default = "";
+        description = "Extra args to run influxd";
+        example = "--pprof-disabled";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    systemd.services.influxdb = {
+      description = "Scalable datastore for metrics, events, and real-time analytics";
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+
+      preScript = ''
+        mkdir -p ${cfg.dataDir}
+      '';
+
+      script = ''
+        ${pkgs.influxdb2}/bin/influxd --reporting-disabled \
+          --bolt-path ${cfg.dataDir}/influxd.bolt --engine-path ${cfg.dataDir}/engine
+      '';
+    };
+  };
+}
