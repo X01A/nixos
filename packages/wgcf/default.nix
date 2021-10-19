d21f73b68fae893bcaa83c388611cd3138e7157c
Indexyz <jiduye@gmail.com>
2021-10-19 14:29:56 +0000

    fix: wgcf vendor sha256

--- packages/wgcf/default.nix
+++ packages/wgcf/default.nix
@@ -3,7 +3,7 @@
 buildGoModule rec {
   inherit (source) pname version src;
 
-  vendorSha256 = "l1hM+/585tbMvJppzB0zTtc0SQiZTSTxGkX+Os6xzso=";
+  vendorSha256 = "IjX8VAFwdZYjMElZ97bIR41ofmIwvhkyr+/aGofxb+I=";
 
   modSha256 = lib.fakeSha256;
   subPackages = [ "." ];
