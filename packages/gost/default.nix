17188a2d7cce081f7eecb13b46d1e0e8c3736ac2
Indexyz <jiduye@gmail.com>
2021-11-01 13:44:21 +0000

    feat: finish gost package

--- packages/gost/default.nix
+++ packages/gost/default.nix
@@ -3,10 +3,10 @@
 buildGoModule rec {
   inherit (source) pname version src;
 
-  vendorSha256 = lib.fakeSha256;
+  vendorSha256 = "XnD2hYQA+Zs4yuOrIh+nDDDk1JNeWS8vK22538/L7Cs=";
 
   modSha256 = lib.fakeSha256;
-  subPackages = [ "." ];
+  subPackages = [ "cmd/gost" ];
 
   doCheck = false;
 }
