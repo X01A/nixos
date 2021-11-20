b3b09d99dcaee13ca4884c1a6edc031a63df844d
Indexyz <jiduye@gmail.com>
2021-11-20 21:33:09 +0800

    feat: add extraConfig for clash

--- packages/clash-dsl/default.nix
+++ packages/clash-dsl/default.nix
@@ -1,6 +1,6 @@
-{ lib }:
+{ formats, lib }:
 
 {
-  types = import ./types { inherit lib; };
+  types = import ./types { inherit lib formats; };
   combinators = import ./combinators.nix { inherit lib; };
 }
