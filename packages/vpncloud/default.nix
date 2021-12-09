d28d866d8e6e1d50c4da41c8bc507cd0e708f7ec
Indexyz <jiduye@gmail.com>
2021-12-10 05:20:37 +0800

    fix(vpncloud): aarch64 linker config

--- packages/vpncloud/default.nix
+++ packages/vpncloud/default.nix
@@ -3,5 +3,9 @@
 rustPlatform.buildRustPackage rec {
   inherit (source) pname version src;
 
+  prePatch = ''
+    rm .cargo/config.toml
+  '';
+
   cargoSha256 = "eE+RBGdkfH+KMyBfFTMLP2SamwZs/0+MBm5YWiJGAoA=";
 }
