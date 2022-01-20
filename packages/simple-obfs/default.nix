a1b8ba480640996a522512ea2c6a1cdb0e213823
Indexyz <jiduye@gmail.com>
2022-01-20 15:44:21 +0800

    pkgs(simple-obfs): disable documentation

--- packages/simple-obfs/default.nix
+++ packages/simple-obfs/default.nix
@@ -1,8 +1,14 @@
-{ source, stdenv, autoconf, automake, libtool, asciidoc, xmlto, libev, autoreconfHook, ... }:
+{ source, lib, gcc8Stdenv, autoconf, automake, libtool, libev, autoreconfHook, ... }:
 
-stdenv.mkDerivation rec {
+with lib;
+gcc8Stdenv.mkDerivation rec {
   inherit (source) pname version src;
 
+  configureFlags = [ "--disable-documentation" ];
+
   nativeBuildInputs = [ autoconf automake autoreconfHook ];
-  buildInputs = [ libtool asciidoc xmlto libev ];
+  buildInputs = [ libtool libev ];
+
+  # MacOS support is broken
+  meta.platforms = platforms.linux;
 }
