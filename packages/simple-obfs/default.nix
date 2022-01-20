c1ef1e9be9635a99eaf5eb65a3d96eee210bc1dc
Indexyz <jiduye@gmail.com>
2022-01-20 16:28:05 +0800

    feat: update simple obfs to android ver

--- packages/simple-obfs/default.nix
+++ packages/simple-obfs/default.nix
@@ -1,8 +1,18 @@
-{ source, lib, gcc8Stdenv, autoconf, automake, libtool, libev, autoreconfHook, ... }:
+{ source, fetchgit, lib, gcc8Stdenv, autoconf, automake, libtool, libev, autoreconfHook, ... }:
 
 with lib;
 gcc8Stdenv.mkDerivation rec {
-  inherit (source) pname version src;
+  # inherit (source) pname version src;
+  pname = "simple-obfs";
+  version = "test-android";
+  src = fetchgit {
+    url = "https://github.com/shadowsocks/simple-obfs.git";
+    rev = "0c6ea880a49d712aa85ef5fa3acaf9ddceb6c421";
+    sha256 = "sha256-ZqfNrDlkF9464qeYCB8m+8J0twylMDt0E6XQZP51kT4=";
+    fetchSubmodules = true;
+    deepClone = false;
+    leaveDotGit = false;
+  };
 
   configureFlags = [ "--disable-documentation" ];
 
