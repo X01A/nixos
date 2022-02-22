461a72a86d710ef68b1fbf75ad5b94e6fc2a46c2
Indexyz <jiduye@gmail.com>
2022-02-22 19:46:42 +0800

    feat: use 2.1.3 staging zfs for rc kernel

--- packages/os-specific/linux/default.nix
+++ packages/os-specific/linux/default.nix
@@ -1,6 +1,15 @@
 { nixpkgs, nvfetcherOut, kernelPatches, ... }:
 
 with nixpkgs;
+let
+  linux-rc = callPackage ./linux-rc {
+    source = nvfetcherOut.linux-rc;
+    kernelPatches = [
+      kernelPatches.bridge_stp_helper
+      kernelPatches.request_key_helper
+    ];
+  };
+in
 {
   ksmbd-tools = callPackage ./ksmbd/tools.nix {
     source = nvfetcherOut.ksmbd-tools;
@@ -10,11 +19,19 @@ with nixpkgs;
     source = nvfetcherOut.ksmbd;
   };
 
-  linux-rc = callPackage ./linux-rc {
-    source = nvfetcherOut.linux-rc;
-    kernelPatches = [
-      kernelPatches.bridge_stp_helper
-      kernelPatches.request_key_helper
-    ];
-  };
+  inherit linux-rc;
+
+  linuxPackages-rc = (linuxPackagesFor linux-rc).extend (final: prev: {
+    zfs = prev.zfs.overrideAttrs (_: {
+      name = "zfs-2.1.3-staging-${prev.kernel.version}";
+      version = "zfs-2.1.3-staging";
+      src = fetchFromGitHub {
+        owner = "openzfs";
+        repo = "zfs";
+        rev = "zfs-2.1.3-staging";
+        sha256 = "sha256-bQ6KR3XlqIze4esHpfWZTBXTdtM8y67Tg+jQlLG7HD4";
+      };
+      meta.broken = false;
+    });
+  });
 }
