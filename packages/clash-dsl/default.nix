83b25408a8fd855ebf0384fcd335764d29f93d23
Indexyz <jiduye@gmail.com>
2022-04-02 05:38:29 +0800

    clash-dsl: add wrapperRuleFile

--- packages/clash-dsl/default.nix
+++ packages/clash-dsl/default.nix
@@ -1,6 +1,8 @@
 { formats, lib }:
 
-{
+rec {
   types = import ./types { inherit lib formats; };
   combinators = import ./combinators.nix { inherit lib formats; };
+
+  wapperRulesFile = rulesFile: (combinators.wapperRules (import rulesFile { inherit combinators; }));
 }
