6d74fff7382c0006e4b64bcd5f3715e5af29ff91
Indexyz <jiduye@gmail.com>
2022-04-27 17:27:06 +0800

    fix: gost hash error

--- packages/gost/default.nix
+++ packages/gost/default.nix
@@ -3,7 +3,7 @@
 buildGoModule rec {
   inherit (source) pname version src;
 
-  vendorSha256 = "sha256-NGlV/qcnUlNLvt3uVRdfx+lUDgqAEBEowW9WIHBY+AI=";
+  vendorSha256 = "sha256-594s/JRhK+n7ZupeUuNwqb05n3nUg0shAvKntFm2qYY=";
 
   modSha256 = lib.fakeSha256;
   subPackages = [ "cmd/gost" ];
