8a2a5772d3b1eb664e19a5747f4ee0866ceb2d26
Indexyz <jiduye@gmail.com>
2022-04-27 17:31:16 +0800

    fix: wgcf

--- packages/wgcf/default.nix
+++ packages/wgcf/default.nix
@@ -3,7 +3,7 @@
 buildGoModule rec {
   inherit (source) pname version src;
 
-  vendorSha256 = "sha256-93NRtqtpBZ/6jj3Vo9OdVowIjCkNKVv4bZelPcgefTE=";
+  vendorSha256 = "sha256-NGlV/qcnUlNLvt3uVRdfx+lUDgqAEBEowW9WIHBY+AI=";
 
   modSha256 = lib.fakeSha256;
   subPackages = [ "." ];
