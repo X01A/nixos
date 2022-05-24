17f0aedd7a7c5224908a1febfc4851cb96e697bc
Indexyz <jiduye@gmail.com>
2022-05-24 10:54:37 +0800

    fix: update gost sha256

--- packages/gost/default.nix
+++ packages/gost/default.nix
@@ -3,7 +3,7 @@
 buildGoModule rec {
   inherit (source) pname version src;
 
-  vendorSha256 = "sha256-594s/JRhK+n7ZupeUuNwqb05n3nUg0shAvKntFm2qYY=";
+  vendorSha256 = "sha256-niLw6rZ/IwSpdICym/ULbxvKfGQxrGsvzwM80XQvkSw=";
 
   modSha256 = lib.fakeSha256;
   subPackages = [ "cmd/gost" ];
