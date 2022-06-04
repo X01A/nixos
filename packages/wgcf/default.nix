454d855205f6f8e7b4ee0972458c60f6236c5f96
Indexyz <jiduye@gmail.com>
2022-06-04 20:32:41 +0800

    fix: update wgcf hash

--- packages/wgcf/default.nix
+++ packages/wgcf/default.nix
@@ -3,7 +3,7 @@
 buildGoModule rec {
   inherit (source) pname version src;
 
-  vendorSha256 = "sha256-NGlV/qcnUlNLvt3uVRdfx+lUDgqAEBEowW9WIHBY+AI=";
+  vendorSha256 = "sha256-tR/JYF4IF01fLbI2rZvAGnO2bfIaPlPij70U1DPjjJ0=";
 
   modSha256 = lib.fakeSha256;
   subPackages = [ "." ];
