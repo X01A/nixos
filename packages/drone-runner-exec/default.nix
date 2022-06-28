cced70701ecac73323fef4c7bf52cc91c533b9fc
Indexyz <jiduye@gmail.com>
2022-06-29 02:34:58 +0800

    fix: drone-runner-exec vendor hash

--- packages/drone-runner-exec/default.nix
+++ packages/drone-runner-exec/default.nix
@@ -3,7 +3,7 @@
 buildGoModule rec {
   inherit (source) pname version src;
 
-  vendorSha256 = "ypYuQKxRhRQGX1HtaWt6F6BD9vBpD8AJwx/4esLrJsw=";
+  vendorSha256 = "sha256-Atl8jY4h7d1K4lCemX/fwiS3Nc0w56LUPy8ZrdZrqxg=";
 
   modSha256 = lib.fakeSha256;
   subPackages = [ "." ];
