cb8dcd4c8fed724b9d8375e31f3cf5ab572968be
Indexyz <jiduye@gmail.com>
2022-07-22 15:26:17 +0800

    fix: remove modules

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
           pkgs = import nixpkgs {
             inherit system;
             overlays = [
-              rust-overlay.overlay
+              rust-overlay.overlays.default
             ];
           };
 
