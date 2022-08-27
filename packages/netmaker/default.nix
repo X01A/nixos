25dc3a626e856f07fcb9cb1416e0e328cf0ce0cc
Indexyz <jiduye@gmail.com>
2022-08-28 00:23:22 +0800

    feat: update netmaker

--- packages/netmaker/default.nix
+++ packages/netmaker/default.nix
@@ -3,7 +3,7 @@
 buildGoModule rec {
   inherit (source) pname version src;
 
-  vendorSha256 = "sha256-AfD5d0KDnVIEo4glf1IqTT33XvXbnMILhCbyslUdZiQ=";
+  vendorSha256 = "sha256-171/Xx0wvJt4X3KdVZJG/D7BIiqdh0mB/AcQMmLA6/M=";
 
   modSha256 = lib.fakeSha256;
   subPackages = [ "." "netclient" ];
