476f0f05dc314a26062cf3bf8136bc69623691fd
Indexyz <jiduye@gmail.com>
2022-10-20 18:52:44 +0800

    fix: sway brightness control

--- home/programs/sway.nix
+++ home/programs/sway.nix
@@ -18,8 +18,8 @@ let
 
   keybindings = lib.mkOptionDefault
     ({
-      XF86MonBrightnessDown = "exec brightnessctl set 5%-";
-      XF86MonBrightnessUp = "exec brightnessctl set +5%";
+      XF86MonBrightnessDown = "exec 'brightnessctl set 5%-'";
+      XF86MonBrightnessUp = "exec 'brightnessctl set +5%'";
 
       XF86AudioRaiseVolume = "exec 'pactl set-sink-volume @DEFAULT_SINK@ +5%'";
       XF86AudioLowerVolume = "exec 'pactl set-sink-volume @DEFAULT_SINK@ -5%'";
