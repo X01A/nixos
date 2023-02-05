51823df7d7e35810cd4fb57029ce2fb7fc113cc8
5aaee9 <jiduye@gmail.com>
2023-02-05 22:14:54 +0800

    feat: with nix-update

--- packages/pufferpanel/default.nix
+++ packages/pufferpanel/default.nix
@@ -1,25 +1,29 @@
-{ lib, stdenvNoCC, fetchFromGitHub, buildGoModule, source, npmlock2nix, jq }:
+{ lib, stdenvNoCC, fetchFromGitHub, buildGoModule, npmlock2nix, jq }:
 
 let
-  webSrc = stdenvNoCC.mkDerivation {
-    pname = "${source.pname}-web-src";
-    inherit (source) version src;
+  pname = "pufferpanel";
+  version = "v2.6.2";
+  src = fetchFromGitHub ({
+    owner = "PufferPanel";
+    repo = "PufferPanel";
+    rev = "v2.6.2";
+    fetchSubmodules = true;
+    sha256 = "sha256-Mwg4wKhbF1ohXGk2VwZrG5Xc5kdhrg5+Wnf0phv53qU=";
+  });
 
+  webSrc = stdenvNoCC.mkDerivation {
+    pname = "${pname}-web-src";
     phases = [ "buildPhase" ];
     # set missing version
     buildPhase = ''
       cp --no-preserve=mode,ownership -r $src/client $out
-      ${jq}/bin/jq ".version = \"${source.version}\"" $out/package-lock.json > test.json
+      ${jq}/bin/jq ".version = \"${version}\"" $out/package-lock.json > test.json
       rm -f $out/package-lock.json
       mv test.json $out/package-lock.json
     '';
   };
 
   web = npmlock2nix.buildNpmPackage {
-    # pname = "${source.pname}-web";
-    # inherit (source) version;
-    # nodejs = nodejs-14_x;
-
     src = webSrc;
 
     npmBuild = "npm run build";
@@ -28,9 +32,8 @@ let
   };
 in
 buildGoModule rec {
-  inherit (source) pname version src;
-
-  vendorSha256 = "sha256-Q8H4Wbese+1TG6DkLHZJySY76xWpMD/dCFQCB6Zm0Vw=";
+  inherit pname version src;
+  vendorSha256 = "sha256-w3PExVOUQQLyjBvRUVqFfNc131ys4UpH6q/eFVCVye8=";
 
   # modSha256 = lib.fakeSha256;
   subPackages = [ "cmd" ];
