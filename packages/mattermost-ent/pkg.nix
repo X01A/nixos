a37936e64e458723487103f592d27efa7599f68f
5aaee9 <jiduye@gmail.com>
2023-02-11 16:52:03 +0800

    fix: mattermost version

--- packages/mattermost-ent/pkg.nix
+++ packages/mattermost-ent/pkg.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation rec {
   pname = "mattermost-ent";
   version = "7.7.1";
   src = fetchurl {
-    url = "https://releases.mattermost.com/7.7.1/mattermost-7.7.1-linux-amd64.tar.gz";
+    url = "https://releases.mattermost.com/${version}/mattermost-${version}-linux-amd64.tar.gz";
     sha256 = "sha256-4FrvKQdGKfwJM7Oc43kg1Iq4o/OT2/kl89bTKBO4EdQ=";
   };
 
