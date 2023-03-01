84b00d6ae40e511539c7f851d377b09c81eed7a9
5aaee9 <jiduye@gmail.com>
2023-03-01 14:08:01 +0800

    mattemost: update download format

--- update.bash
+++ update.bash
@@ -39,4 +39,4 @@ nix-update --commit --version branch=gh-pages --flake yacd-meta
 
 nix-update --commit --version "$(curl --fail -sI 'https://mikrotik.com/mt_redirect.php?code=winbox64' | grep -i Location | grep -oP '([\d]{1,}\.[\d]{1,})')" --flake winbox
 nix-update --commit --version "$(curl --fail -s 'https://github.com/Dreamacro/clash/releases/tag/premium' | grep -oP "Premium \K([0-9\\.]*)" | head -n 1)" --flake clash-premium
-nix-update --commit --version "$(curl --fail -s 'https://mattermost.com/download/' | grep -oP "mattermost-\K(.*)(?=-linux-amd64.tar.gz)" | head -n 1)" --flake mattermost-ent
+nix-update --commit --version "$(curl --fail -s 'https://mattermost.com/download/' | grep -oP "Latest Release:</strong> \K([0-9\\.]*)(?=</p>)")" --flake mattermost-ent
