7aba15a13becb151ef0476356f664a1cf4fd3192
5aaee9 <jiduye@gmail.com>
2023-03-03 17:25:47 +0800

    quickjspp: init at unstable-2022-07-22

---
+++ packages/quickjspp/pkg.nix
@@ -0,0 +1,16 @@
+{ stdenv, fetchFromGitHub, cmake, pkg-config }:
+
+stdenv.mkDerivation rec {
+  pname = "quickjspp";
+  version = "unstable-2022-07-22";
+
+  src = fetchFromGitHub ({
+    owner = "ftk";
+    repo = "quickjspp";
+    rev = "9cee4b4d27271d54b95f6f42bfdc534ebeaaeb72";
+    fetchSubmodules = true;
+    sha256 = "sha256-7iXL3cBoGbl+/vltjCboz2bWGTo/3hwxPO3IlDwLvJo=";
+  });
+
+  nativeBuildInputs = [ cmake pkg-config ];
+}
