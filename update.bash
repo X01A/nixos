2e6cbc4d06f761d2f53e266825a9a188db654123
5aaee9 <jiduye@gmail.com>
2023-05-11 01:15:58 +0800

    pkgs: remove miui-auto-tasks and deps

--- update.bash
+++ update.bash
@@ -34,10 +34,8 @@ nix-update --commit --version branch --flake simple-obfs
 nix-update --commit --version branch --flake edl
 nix-update --commit --version branch --flake commit-notifier
 nix-update --commit --version branch=release --flake mmdb-ipip
-nix-update --commit --version branch=master --flake miui-auto-task
 nix-update --commit --version branch --flake ksmbd-kernel
 nix-update --commit --version branch --flake ksmbd-tools
-nix-update --commit --version branch --flake onepush
 nix-update --commit --version branch=gh-pages --flake yacd-meta
 nix-update --commit --version branch --flake quickjspp
 nix-update --commit --version branch --flake rapidjson
