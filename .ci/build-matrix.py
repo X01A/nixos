912a0f4211486f6f1f0204b4e7b4de6631b4c7cd
5aaee9 <jiduye@gmail.com>
2023-06-22 03:54:48 +0800

    ci: write new line

--- .ci/build-matrix.py
+++ .ci/build-matrix.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 import os
+import sys
 import json
 
 path = os.popen('nix build --print-out-paths --no-link .#packageList').read().strip()
 
-pkgs = "[]"
+pkgs = []
 
 with open(path, 'r') as file:
-    data = json.load(file)
-    pkgs = json.dumps(list(map(lambda x: ({ "package": x }), data)))
+    pkgs = json.load(file)
+
+
+
+if not os.getenv('GITHUB_OUTPUT'):
+    print("GITHUB_OUTPUT env not found")
+    sys.exit(1)
 
 with open(os.getenv('GITHUB_OUTPUT'), 'w') as file:
-    file.write(f"matrix={pkgs}")
+    file.write(f"matrix={json.dumps(list(map(lambda x: ({ 'package': x }), data)))}\n")
