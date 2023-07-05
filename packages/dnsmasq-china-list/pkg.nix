1f7d8aa59f3191661ce7b0363318fd6441f90e67
5aaee9 <7685264+5aaee9@users.noreply.github.com>
2023-07-05 07:17:15 +0000

    dnsmasq-china-list: init

---
+++ packages/dnsmasq-china-list/pkg.nix
@@ -0,0 +1,28 @@
+{
+  stdenvNoCC,
+  fetchFromGitHub,
+  defaultResolver ? "https://1.1.1.1/dns-query",
+  chinaDns ? "114.114.114.114",
+  ...
+}:
+
+stdenvNoCC.mkDerivation {
+  pname = "dnsmasq-china-list";
+  version = "unstable-2023-07-04";
+  src = fetchFromGitHub ({
+    owner = "felixonmars";
+    repo = "dnsmasq-china-list";
+    rev = "dd39484db6645d7f1d27d5f6cf3066c01f883c7b";
+    fetchSubmodules = true;
+    sha256 = "sha256-QvgoyPu7TQbrqQqSHg+C9yn4IdZzn0qyaNBCntaoG0Q=";
+  });
+
+  phases = "installPhase";
+  installPhase = ''
+    mkdir $out
+    echo "${defaultResolver}" > $out/adguard.upstream
+    cat $src/accelerated-domains.china.conf | sed -E "s#server=(\/.+\/)114\.114\.114\.114#[\1]${chinaDns}#g" >> $out/adguard.upstream
+
+    cp $src/accelerated-domains.china.conf $out/
+  '';
+}
