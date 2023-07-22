a236f1037b365889321d960653568937f4353e90
5aaee9 <jiduye@gmail.com>
2023-07-22 20:02:05 +0800

    pkgs(firefox): add 1password

--- packages/index-firefox/pkg.nix
+++ packages/index-firefox/pkg.nix
@@ -66,6 +66,12 @@ wrapFirefox firefox-unwrapped {
         installation_mode = "force_installed";
         install_url = "https://addons.mozilla.org/firefox/downloads/latest/tabby-window-tab-manager/latest.xpi";
       };
+
+      # 1Password
+      "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
+        installation_mode = "force_installed";
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
+      };
     };
   };
 }
