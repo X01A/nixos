19fdf6f630635a45cea8895ee9cf224698565d0f
5aaee9 <jiduye@gmail.com>
2023-07-24 10:35:40 +0800

    realm: try fix build

--- packages/realm/pkg.nix
+++ packages/realm/pkg.nix
@@ -1,7 +1,7 @@
 { fetchFromGitHub, rust-bin, makeRustPlatform, lib, pkg-config, openssl, ... }:
 
 let
-  rust = rust-bin.selectLatestNightlyWith (toolchain: toolchain.default);
+  rust = rust-bin.nightly."2023-06-27".default;
   rustPlatform = makeRustPlatform {
     rustc = rust;
     cargo = rust;
