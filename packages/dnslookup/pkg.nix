8acccc993050b940acdc440e7c21ba575a30d978
5aaee9 <7685264+5aaee9@users.noreply.github.com>
2023-10-19 15:10:05 +0800

    pkgs: add dnslookup

---
+++ packages/dnslookup/pkg.nix
@@ -0,0 +1,20 @@
+{ lib, buildGoModule, fetchFromGitHub }:
+
+buildGoModule rec {
+  pname = "dnslookup";
+  version = "1.10.0";
+  src = fetchFromGitHub ({
+    owner = "ameshkov";
+    repo = "dnslookup";
+    rev = "v${version}";
+    fetchSubmodules = true;
+    sha256 = "sha256-T3KvaNAPS4WEALttMaZx+zOc3Iu9aSujH0WNfHa2baM=";
+  });
+
+  vendorSha256 = "sha256-pLgXsasWD4Wb1dW0+1fl5M+CM2hr7zohXiHLsDj73h0=";
+
+  modSha256 = lib.fakeSha256;
+  subPackages = [ "." ];
+
+  doCheck = false;
+}
