e9fcc42393f9e9f84a05b73af8530718a80d5f25
5aaee9 <7685264+5aaee9@users.noreply.github.com>
2024-01-27 21:50:24 +0800

    teleport-ent: 8.0.0 -> 14.3.3

--- packages/teleport-ent/pkg.nix
+++ packages/teleport-ent/pkg.nix
@@ -3,11 +3,11 @@
 stdenv.mkDerivation rec {
   # inherit (source) pname version src;
   pname = "teleport-ent";
-  version = "8.0.0";
+  version = "14.3.3";
 
   src = fetchurl {
-    url = "https://get.gravitational.com/teleport-ent-v8.0.0-linux-amd64-bin.tar.gz";
-    sha256 = "0gabah8q460ydjrqg5z16n0ar5diz69g03ccqshfdz298ad39g3q";
+    url = "https://cdn.teleport.dev/teleport-ent-v${version}-linux-amd64-bin.tar.gz";
+    sha256 = "sha256-73IhB6KrLrO74SZfyYfl9fXyQ6fFvcwgmGWuHLS8b1E=";
   };
 
   inherit buildPhase;
