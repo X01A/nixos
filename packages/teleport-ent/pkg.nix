4f8866a06b5a2d6bbb630001d00289e4ec498bd5
Indexyz <7685264+5aaee9@users.noreply.github.com>
2024-02-19 11:48:35 +0800

    teleport: update arm64 hash

--- packages/teleport-ent/pkg.nix
+++ packages/teleport-ent/pkg.nix
@@ -10,7 +10,7 @@ let
     };
     aarch64-linux = {
       url = "https://cdn.teleport.dev/teleport-ent-v${version}-linux-arm64-bin.tar.gz";
-      sha256 = "sha256-C4jfht74/GZVLgubDRtOcnawsJYywN2h7lJW6PGMPCY=";
+      sha256 = "sha256-XGT7OlvSxcIaqeO84g4L+QFEzuZdF7nGS+UVqjz3vww=";
     };
   }."${stdenv.system}" or (throw "Unsupported system");
 in
