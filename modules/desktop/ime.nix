c5aa926247bbcc46e3d5b5a5540ad94548cf033d
Indexyz <7685264+5aaee9@users.noreply.github.com>
2024-09-30 12:51:36 +0800

    fix: ime error

--- modules/desktop/ime.nix
+++ modules/desktop/ime.nix
@@ -13,8 +13,7 @@ in
 
   config = mkIf cfg.enable {
     i18n.inputMethod = {
-      enable = true;
-      type = "fcitx5";
+      enabled = "fcitx5;
       fcitx5.addons = with pkgs; [
         fcitx5-rime
         fcitx5-material-color
