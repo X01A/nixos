7013128683948fed25f1819701730a0308ba9db0
Indexyz <7685264+5aaee9@users.noreply.github.com>
2024-09-30 12:54:02 +0800

    fix: err

--- modules/desktop/ime.nix
+++ modules/desktop/ime.nix
@@ -13,7 +13,7 @@ in
 
   config = mkIf cfg.enable {
     i18n.inputMethod = {
-      enabled = "fcitx5;
+      enabled = "fcitx5";
       fcitx5.addons = with pkgs; [
         fcitx5-rime
         fcitx5-material-color
