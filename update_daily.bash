129dbb2f423fa4d755d6635acc53cb511ab52f2c
Indexyz <7685264+5aaee9@users.noreply.github.com>
2024-10-04 16:49:01 +0800

    feat: remove clash meta

--- update_daily.bash
+++ update_daily.bash
@@ -9,7 +9,6 @@ nix-update --commit --version branch=gh-pages --flake yacd-meta
 nix-update --commit --version branch --flake quickjspp
 nix-update --commit --version branch --flake rapidjson
 nix-update --commit --version branch --flake libcron
-nix-update --commit --version branch=Alpha --flake clash-meta
 nix-update --commit --version branch --flake dnsmasq-china-list
 nix-update --commit --version branch --flake chnroutes
 nix-update --commit --version branch --flake stickerpicker
