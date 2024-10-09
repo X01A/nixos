09f446b45375d057ced5595ad42d2940fb2fd3f3
Indexyz <7685264+5aaee9@users.noreply.github.com>
2024-10-10 02:23:25 +0800

    feat: remove downstream winbox

--- update.bash
+++ update.bash
@@ -32,7 +32,6 @@ nix-update --commit --flake ping-exporter
 nix-update --commit --flake mumble-discord-bridge
 nix-update --commit --flake aliyundrive-webdav
 
-nix-update --commit --version "$(curl --fail -sI 'https://mikrotik.com/mt_redirect.php?code=winbox64' | grep -i Location | grep -oP '([\d]{1,}\.[\d]{1,})')" --flake winbox
 nix-update --commit --version "$(curl --fail -s 'https://mattermost.com/download/' | grep -oP "Latest Release:</strong> \K([0-9\\.]*)(?=</p>)")" --flake mattermost-ent
 
 for package in "teleport-ent"; do
