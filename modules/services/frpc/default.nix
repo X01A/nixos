80f7c31c07fe9ace4f108030d54ab6d60ced79da
Indexyz <7685264+5aaee9@users.noreply.github.com>
2025-01-30 07:23:21 +0800

    feat: update frpc

--- modules/services/frpc/default.nix
+++ modules/services/frpc/default.nix
@@ -3,60 +3,9 @@
 with lib;
 let
   cfg = config.indexyz.services.frpc;
-  certs = config.security.acme.certs;
-  serverOptions = types.submodule (import ./server-options.nix {
-    inherit lib;
-  });
+  format = pkgs.formats.toml { };
 
-  tunnelConfig = item: ''
-    [${item.name}]
-    type = ${item.type}
-
-    ${optionalString (item.type != "https") ''
-    local_ip = ${item.host}
-    local_port = ${toString item.port}
-    ''}
-
-    ${optionalString (item.type == "tcp" || item.type == "udp") ''
-    remote_port = ${toString item.remotePort}
-    ''}
-    ${optionalString (item.type == "http" || item.type == "https") ''
-    custom_domains = ${item.domain}
-    ''}
-
-    ${optionalString (item.type == "https") ''
-    plugin = https2http
-    plugin_local_addr = ${item.host}:${toString item.port}
-    ''}
-
-    ${optionalString (item.type == "https" || item.acme) ''
-    plugin_crt_path = ${certs.${item.domain}.directory}/fullchain.pem
-    plugin_key_path = ${certs.${item.domain}.directory}/key.pem
-    ''}
-  '';
-
-  generateAfter = server: map
-    (item: "acme-finished-${item.domain}.target")
-    (filter (item: item.acme) server.rules);
-
-  basicConfig = server: ''
-    [common]
-    tls_enable = true
-    server_addr = ${server.server}
-    server_port = ${toString server.serverPort}
-    token = ${server.token}
-  '';
-
-
-  cfgFile = server: pkgs.writeText "frpc-${server.name}.ini" ''
-    ${basicConfig server}
-
-    ${concatStringsSep "\n" (map tunnelConfig server.rules)}
-  '';
-
-  acmeHosts = map
-    (item: item.domain)
-    (filter (item: item.type == "https" || item.acme) (lib.lists.flatten (map (item: item.rules) cfg.servers)));
+  cfgFile = format.generate "config.toml" cfg.settings;
 in
 {
   options = {
@@ -66,33 +15,24 @@ in
         type = with types; bool;
       };
 
-      servers = mkOption {
-        default = [ ];
-        type = types.listOf serverOptions;
+      settings = mkOption {
+        type = format.type;
+        default = { };
       };
     };
   };
 
   config = mkIf cfg.enable {
-    # TODO: request cert sign if host type https
+    systemd.services.frpc = {
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
 
-    systemd.services = builtins.listToAttrs (map
-      (item: {
-        name = "frpc-${item.name}";
-        value = {
-          wantedBy = [ "multi-user.target" ];
-          after = [ "network.target" ] ++ (generateAfter item);
-
-          startLimitIntervalSec = 0;
-          serviceConfig = {
-            ExecStart = "${pkgs.frp}/bin/frpc -c ${cfgFile item}";
-            Restart = "always";
-            RestartSec = "10s";
-          };
-        };
-      })
-      cfg.servers);
-
-    environment.etc."hosts.json".text = builtins.toJSON acmeHosts;
+      startLimitIntervalSec = 0;
+      serviceConfig = {
+        ExecStart = "${pkgs.frp}/bin/frpc -c ${cfgFile}";
+        Restart = "always";
+        RestartSec = "10s";
+      };
+    };
   };
 }
