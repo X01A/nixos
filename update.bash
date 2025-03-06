eb488ee5419539251e64fddf18ff7b807784b3d7
5aaee9 <7685264+5aaee9@users.noreply.github.com>
2025-03-06 08:06:00 +0000

    fix: update

--- update.bash
+++ update.bash
@@ -31,6 +31,7 @@ nix-update --commit --flake reader
 nix-update --commit --flake ping-exporter
 nix-update --commit --flake mumble-discord-bridge
 nix-update --commit --flake aliyundrive-webdav
+nix-update --commit --flake alist
 
 nix-update --commit --version "$(curl --fail -s 'https://mattermost.com/download/' | grep -oP "Latest Release:</strong> \K([0-9\\.]*)(?=</p>)")" --flake mattermost-ent
 
@@ -46,6 +47,3 @@ for package in "teleport-ent"; do
     git commit -m "$package: $current_version -> $new_version"
   fi
 done
-
-# nix-update -commit --flake teleport-ent
-nix-update --commit --flake alist
