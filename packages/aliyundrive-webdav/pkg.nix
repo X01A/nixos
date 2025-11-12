11bee8c5cf2fe969382a1386c787ca828cb6ad1c
Indexyz <7685264+5aaee9@users.noreply.github.com>
2025-11-12 15:10:26 +0800

    fix: disable cache

--- packages/aliyundrive-webdav/pkg.nix
+++ packages/aliyundrive-webdav/pkg.nix
@@ -11,5 +11,5 @@ rustPlatform.buildRustPackage rec {
     sha256 = "sha256-LN2HdmhcPk6RCrRZdV3A8InoYgqJSHC2XjjuKZLfVaI=";
   });
 
-  cargoHash = "sha256-/UZHfAW96kqySmic9Fc2FDOq7QAfcVHo1Stlx67YeS8=";
+  cargoHash = "sha256-eow/kyjruEiT5QSGge09kKA/yS74C5BWjg+xRZUkmLQ=";
 }
--- update_daily.bash
+++ update_daily.bash
@@ -12,3 +12,4 @@ nix-update --commit --version branch --flake libcron
 nix-update --commit --version branch --flake dnsmasq-china-list
 nix-update --commit --version branch --flake chnroutes
 nix-update --commit --version branch --flake stickerpicker
+nix-update --commit --version branch --flake asterisk-chan-quectel
