cf0007f977b1e60b38cab76459c845330c9f9aad
Indexyz <7685264+5aaee9@users.noreply.github.com>
2026-04-13 18:19:37 +0800

    feat: update

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -20,12 +20,12 @@ in
   config = mkIf cfg.enable {
     programs.git = {
       enable = true;
-      user = { 
-        email = "7685264+5aaee9@users.noreply.github.com";
-        name = "Indexyz";
-      };
       signing.format = "openpgp";
       settings = {
+        user = {
+          email = "7685264+5aaee9@users.noreply.github.com";
+          name = "Indexyz";
+        };
         credential = {
           "https://github.com" = {
             helper = "${pkgs.gh}/bin/gh auth git-credential";
