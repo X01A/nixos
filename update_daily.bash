d738357a6b7f5b64a049f48172c6974696a8c88f
Indexyz <7685264+5aaee9@users.noreply.github.com>
2026-04-13 18:10:02 +0800

    feat: remove upstreamed rapidjson

--- update_daily.bash
+++ update_daily.bash
@@ -7,7 +7,6 @@ nix-update --commit --version branch --flake ksmbd-kernel
 nix-update --commit --version branch --flake ksmbd-tools
 nix-update --commit --version branch=gh-pages --flake yacd-meta
 nix-update --commit --version branch --flake quickjspp
-nix-update --commit --version branch --flake rapidjson
 nix-update --commit --version branch --flake libcron
 nix-update --commit --version branch --flake dnsmasq-china-list
 nix-update --commit --version branch --flake chnroutes
