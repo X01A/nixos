3dfef70d1b0e83818244390ed37a16302e3a5ae5
5aaee9 <7685264+5aaee9@users.noreply.github.com>
2026-07-01 15:00:06 +0800

    feat(modules): generate NixOS module imports

--- modules/all-modules.nix
+++ modules/all-modules.nix
@@ -1,48 +1,25 @@
-{ ... }:
+{ lib, ... }:
 
-{
-  imports = [
-    ./environment/base.nix
-    ./environment/qemu.nix
-    ./environment/efi.nix
-    ./environment/shell.nix
-    ./environment/acme
-    ./services/ssh.nix
-    ./services/clash
-    ./services/teleport-agent.nix
-    ./services/drone-web.nix
-    ./services/drone-docker.nix
-    ./services/frpc
-    ./services/frps
-    ./services/k3s.nix
-    ./services/transmission.nix
-    ./services/libvirt
-    ./services/atftpd.nix
-    ./services/novnc.nix
-    ./services/leaf.nix
-    ./services/dhcp
-    ./services/dns
-    ./services/forwarding
-    ./services/hpool-miner.nix
-    ./services/prometheus.nix
-    ./services/vlmcsd.nix
-    ./services/ksmbd.nix
-    ./services/cloudreve.nix
-    ./services/vouch.nix
-    ./services/nginx.nix
-    ./services/argo.nix
-    ./services/drone-exec.nix
-    ./services/vpncloud.nix
-    ./services/influxdb.nix
-    ./devices/mlx4-mode.nix
-    ./desktop/font.nix
-    ./desktop/ime.nix
-    ./desktop/sway.nix
-    ./system/uboot
-    ./services/wiretrustee-client.nix
-    ./services/pufferpanel.nix
-    ./network/nftables/default.nix
-    ./network/tailscale
-    ./network/netclient.nix
+let
+  moduleLib = import ../lib/modules.nix {
+    inherit lib;
+    self.attrs = import ../lib/attrs.nix { inherit lib; self = { }; };
+  };
+
+  excludedEntrypoints = [
+    # Requires a deployment module declaring deployment.*; unsafe in plain NixOS eval.
+    ./services/secrets
   ];
+
+  generatedEntrypoints = moduleLib.moduleEntrypointsFromDirs ./. [
+    "environment"
+    "services"
+    "devices"
+    "desktop"
+    "system"
+    "network"
+  ];
+in
+{
+  imports = lib.subtractLists excludedEntrypoints generatedEntrypoints;
 }
