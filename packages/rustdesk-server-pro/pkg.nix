fcabef570b3c0c92fe6873fce18b0d16285f3d6a
5aaee9 <7685264+5aaee9@users.noreply.github.com>
2026-07-30 16:22:49 +0800

    chore(rustdesk-server-pro): update to 1.8.5

--- packages/rustdesk-server-pro/pkg.nix
+++ packages/rustdesk-server-pro/pkg.nix
@@ -6,11 +6,11 @@
 
 stdenv.mkDerivation rec {
   pname = "rustdesk-server-pro";
-  version = "1.8.2";
+  version = "1.8.5";
 
   src = fetchurl {
     url = "https://github.com/rustdesk/rustdesk-server-pro/releases/download/${version}/rustdesk-server-linux-amd64.tar.gz";
-    sha256 = "sha256-XdlBC1MFg8IxLmAzlVsfDAzwIGH/qd6cu03+ZEz3Zhs=";
+    sha256 = "sha256-/oT8gH763G9hfFyc5BvmVsRx85+DerCuZoFa+fCZYCw=";
   };
 
   nativeBuildInputs = [
